56e7f118532bdb3da703bc912acb2c70c29fe1b3
yassin <saied.yassin@gmail.com>
2023-05-04 03:16:39 +0200

    first commit

---
+++ src/Controller/DashbordController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashbordController extends AbstractController
+{
+    #[Route('/dashboard', name: 'cmpt_dashbord')]
+    public function index(): Response
+    {
+        return $this->render('dashbord/index.html.twig', [
+            'controller_name' => 'DashbordController',
+        ]);
+    }
+}
