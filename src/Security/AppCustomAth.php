2159aa8e4c21dd24b10e9f38bf5b58334761d2e0
yassin <saied.yassin@gmail.com>
2023-05-08 02:54:43 +0200

    Custom Auth done

--- src/Security/AppCustomAth.php
+++ src/Security/AppCustomAth.php
@@ -4,31 +4,58 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 
 class AppCustomAth extends AbstractAuthenticator
 {
+
+    private RouterInterface $router;
+
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+
+
     public function supports(Request $request): ?bool
     {
         return ($request->getPathInfo() === '/login' && $request->isMethod('POST'));
     }
 
-    public function authenticate(Request $request): PassportInterface
+    public function authenticate(Request $request): Passport
     {
-        dd('authenticate!');
+        $password = $request->request->get('_password');
+        $email = $request->request->get('_username');
+        $csrfToken = $request->request->get('_csrf_token');
+
+        return new Passport(
+            new UserBadge($email),
+            new PasswordCredentials($password),
+            [new CsrfTokenBadge('authenticate', $csrfToken)]
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse(
+
+            $this->router->generate('cmpt_dashbord')
+        );
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        dd('fail');
     }
 
     //    public function start(Request $request, AuthenticationException $authException = null): Response
