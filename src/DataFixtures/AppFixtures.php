380fefa2da4fa985c1386e62bbeaed0743960cc0
yassin <saied.yassin@gmail.com>
2023-05-29 01:16:43 +0200

    create account Categories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,17 +3,49 @@
 namespace App\DataFixtures;
 
 use App\Factory\UserFactory;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\AccountBalance;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+
+    const ACCOUNT = [
+        "Actifs" => ["Actifs non courants ", "Actifs courants"],
+        "Passive" => ["Capitaux propres", "Passive courants"]
+
+    ];
+
+
+
+
+
+
+
+
+
+
+    // public function load(ObjectManager $manager): void
+    // {
+    //     // $product = new Product();
+    //     // $manager->persist($product);
+    //     UserFactory::createOne(['email' => 'admin@admin.com']);
+    //     UserFactory::createMany(10);
+    //     $manager->flush();
+    // }
+
+    function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-        UserFactory::createOne(['email' => 'admin@admin.com']);
-        UserFactory::createMany(10);
+
+        $accountBalance = new AccountBalance();
+
+        foreach (self::ACCOUNT as $key => $value) {
+            $accountBalance->setAccount($key);
+            $accountBalance->addAccountType($value);
+        }
+
+        $manager->persist($accountBalance);
         $manager->flush();
     }
 }
