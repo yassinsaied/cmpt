31980f5eea6e6b9b642ea9fcd655cb8a99a1e400
yassinsaied <saied.yassin@gmail.com>
2023-06-01 17:27:44 +0100

    updating data with Event Listener in Acoount form

--- src/Form/AccountBalanceType.php
+++ src/Form/AccountBalanceType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\AccountBalance;
 use Doctrine\ORM\EntityRepository;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AccountBalanceType extends AbstractType
-{
+{   
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -28,37 +29,46 @@ class AccountBalanceType extends AbstractType
                 'label' => 'Code',
             ])
 
-            ->add('account', EntityType::class, [
+           ->add('account', EntityType::class, [
                 'class' => AccountBalance::class,
                 'query_builder' => function (EntityRepository $er) {
                     return $er->findAccountByStatus(0);
                 },
                 'choice_label' => 'name',
                 'placeholder' => 'Choose an Account',
+                'mapped' => false
             ])
 
-
-
             ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
-                 $product = $event->getData();
-                 $form = $event->getForm();
-        
-            //    dd($product  ) ;
 
-            if (!$product || null === $product->getId()) {
-                $form->add('accountType', EntityType::class, [
-                    'class' => AccountBalance::class,
-                    'query_builder' => function (EntityRepository $er) {
-                        return $er->findAccountByStatus(1);
-                    },
-                    'choice_label' => 'name',
-                    'placeholder' => 'Choose an AccountType',
-                    'mapped' => false
-                ]);
-            }
+                 $data = $event->getData();
+                 $form = $event->getForm();
+
+                 $account = $form->get("account")->getData();
+                 $accountType = null === $account ? [] : $account->getAccountType();
+
+              
+                    
+               // Add the Neighborhoods field with the properly data
+                    $form->add('accountType', EntityType::class, [
+                        'class' => AccountBalance::class,
+                        'choices' => $accountType,
+                        'choice_label' => 'name',
+                        'placeholder' => 'Choose an AccountType',
+                        'mapped' => false
+                    ]);
+                
+
+    
+          
             })
             ->add('save', SubmitType::class);
     }
+
+    
+  
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
