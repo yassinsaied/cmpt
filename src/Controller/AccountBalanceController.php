5f6d76fe2744589b931fd145c3c75dcfc1005743
yassinsaied <saied.yassin@gmail.com>
2023-06-05 17:35:56 +0100

    Crud of accountBlance in progress

--- src/Controller/AccountBalanceController.php
+++ src/Controller/AccountBalanceController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\AccountBalance;
 use App\Form\AccountBalanceType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +13,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AccountBalanceController extends AbstractController
 {
     #[Route('/account/create', name: 'cmpt_create_account')]
-    public function createAccount(Request $request): Response
+    public function createAccount(Request $request , EntityManagerInterface $entityManager): Response
     {
+        $accountBalance = new AccountBalance() ;
 
-
-        $form = $this->createForm(AccountBalanceType::class, new AccountBalance());
+        $form = $this->createForm(AccountBalanceType::class, $accountBalance);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $accountBalance->setStatus(3);
+            $accountBalance->setAccount($form->get('subAccount')->getData());
+            $entityManager->persist( $accountBalance) ;
+            $entityManager->flush() ;
+        }
 
         return $this->render('account_balance/create.html.twig', [
              "formCreateAccount" =>  $form->createView() 
         ]);
     }
+
+
 }
